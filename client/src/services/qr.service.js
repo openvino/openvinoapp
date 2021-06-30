dfd62554fcbd9be40f66afad22a17e60b1e792cc
Luis Videla <luisvid@gmail.com>
2021-06-30 10:07:53 -0300

    response not a correct

--- client/src/services/qr.service.js
+++ client/src/services/qr.service.js
@@ -5,15 +5,25 @@ const API_URL = "http://165.232.157.193:3000";
 
 class QrService {
 
-    async checkQR(qrValue) {
+    async checkQR_old(qrValue) {
         try {
             const response = await axios.get(API_URL + "/qrstatus/" + qrValue);
-            console.log(response);
+            console.log(response.data.allowClaim);
         } catch (error) {
             console.log(error);
         }
-
     }
+
+    async checkQR(qrValue) {
+        return axios
+        .get(API_URL + "/qrstatus/" + qrValue)
+        .then (response => {
+            console.log("response.data");
+            console.log(response.data);
+            console.log(response.data.allowClaim);
+        });
+    }
+
 
 }
 
