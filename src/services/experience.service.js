e25fd8a75318d97ee26b46ddeb5404b1ea4e73e3
Luis Videla <luisvid@gmail.com>
2022-02-05 09:37:32 -0300

    added url from env file

--- src/services/experience.service.js
+++ src/services/experience.service.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://143.198.152.77" 
+// const API_URL = "http://143.198.152.77" 
+const API_URL = process.env.APPLICATION_URL;
   
 
 class ExperienceService {
