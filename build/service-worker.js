c2265a5417da09f704b818ae8c2e8d4d104d1d64
fpiantoni <fran@piantoni.com.ar>
2022-04-14 11:04:29 -0300

    Fixed API url + Build updated

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.11345af4087553730a274d574b099ef7.js"
+  "/precache-manifest.e37f46ac9672912574ee322ee9c62ea8.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/services/experience.service.js
+++ src/services/experience.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import authHeader from "./auth-header";
 
 //const API_URL = "http://143.198.152.77:4000" 
-const API_URL = "http://159.203.169.184/:3000"
+const API_URL = "http://159.203.169.184:3000"
 
 class ExperienceService {
 
