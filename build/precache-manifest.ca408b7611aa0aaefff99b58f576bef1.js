4a9f0b1d2e5a4a93dff545c4ba2dcabb4dde2f3d
Juan Arguello <arguellojuan08@gmail.com>
2023-09-23 21:43:32 -0300

    ignore build

---
+++ build/precache-manifest.ca408b7611aa0aaefff99b58f576bef1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae26bae53c4801241acc5edd60c76e79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aca1465557e74908ca2",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6c920635522d3d8da458",
+    "url": "/static/css/main.cdf117ed.chunk.css"
+  },
+  {
+    "revision": "3aca1465557e74908ca2",
+    "url": "/static/js/2.281ba829.chunk.js"
+  },
+  {
+    "revision": "bd41cc56560cf97471a25768219246ae",
+    "url": "/static/js/2.281ba829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c920635522d3d8da458",
+    "url": "/static/js/main.8e14a796.chunk.js"
+  },
+  {
+    "revision": "405a0d9530fd470eb51a",
+    "url": "/static/js/runtime-main.e562068f.js"
+  },
+  {
+    "revision": "f4404720ece11355df318a0acf525cb1",
+    "url": "/static/media/loading.f4404720.gif"
+  },
+  {
+    "revision": "3e59f293515a99ec359fca7d4d5bd10e",
+    "url": "/static/media/website-logo.3e59f293.png"
+  }
+]);
