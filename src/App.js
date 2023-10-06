59fdb6422858de5fcf3013e6ce413fb1d54fdd9c
Juan Arguello <arguellojuan08@gmail.com>
2023-10-06 02:10:28 -0300

    add tastings button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import logo from './assets/images/website-logo.png';
-import { withRouter } from 'react-router-dom';
-import AuthService from './services/auth.service';
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import BoardUser from './components/UserBoard';
-import BoardModerator from './components/ModBoard';
-import BoardAdmin from './components/AdminBoard';
-import NewExperience from './components/NewExperience';
-import EditProfile from './components/EditProfile';
-import SingleExperience from './components/SingleExperience';
-import ForgotPassword from './components/ForgotPassword';
-import UpdatePassword from './components/UpdatePassword';
-import i18next from 'i18next';
-import i18n from './i18n';
-import { withTranslation } from 'react-i18next';
-import Select from 'react-validation/build/select';
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import logo from "./assets/images/website-logo.png";
+import { withRouter } from "react-router-dom";
+import AuthService from "./services/auth.service";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import BoardUser from "./components/UserBoard";
+import BoardModerator from "./components/ModBoard";
+import BoardAdmin from "./components/AdminBoard";
+import NewExperience from "./components/NewExperience";
+import EditProfile from "./components/EditProfile";
+import SingleExperience from "./components/SingleExperience";
+import ForgotPassword from "./components/ForgotPassword";
+import UpdatePassword from "./components/UpdatePassword";
+import i18next from "i18next";
+import i18n from "./i18n";
+import { withTranslation } from "react-i18next";
+import Select from "react-validation/build/select";
 
 class App extends Component {
   constructor(props) {
@@ -31,8 +31,8 @@ class App extends Component {
       showModeratorBoard: false,
       showAdminBoard: false,
       currentUser: undefined,
-      test: '',
-      value: 'en',
+      test: "",
+      value: "en",
     };
   }
 
@@ -52,15 +52,15 @@ class App extends Component {
   }
 
   handleChange = (event) => {
-    console.log('selected val is ', event.target.value);
+    console.log("selected val is ", event.target.value);
     let newlang = event.target.value;
     this.setState((prevState) => ({ value: newlang }));
-    console.log('state value is', newlang);
+    console.log("state value is", newlang);
     this.props.i18n.changeLanguage(newlang);
   };
 
   render() {
-    console.log('Polygon Version');
+    console.log("Polygon Version");
     const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
     const changeLanguage = (lng) => {
       i18n.changeLanguage(lng);
@@ -70,41 +70,41 @@ class App extends Component {
         <nav className="navbar navbar-expand" id="top-header">
           <div className="language-selector">
             <li className="nav-item">
-              <button className="item" onClick={() => changeLanguage('es')}>
+              <button className="item" onClick={() => changeLanguage("es")}>
                 ES
               </button>
             </li>
             <li className="nav-item">|</li>
             <li className="nav-item">
-              <button className="item" onClick={() => changeLanguage('en')}>
+              <button className="item" onClick={() => changeLanguage("en")}>
                 EN
               </button>
             </li>
             <li className="nav-item">|</li>
 
             <li className="nav-item">
-              <button className="item" onClick={() => changeLanguage('fr')}>
+              <button className="item" onClick={() => changeLanguage("fr")}>
                 FR
               </button>
             </li>
             <li className="nav-item">|</li>
 
             <li className="nav-item">
-              <button className="item" onClick={() => changeLanguage('cat')}>
+              <button className="item" onClick={() => changeLanguage("cat")}>
                 CAT
               </button>
             </li>
             <li className="nav-item">|</li>
 
             <li className="nav-item">
-              <button className="item" onClick={() => changeLanguage('pr')}>
+              <button className="item" onClick={() => changeLanguage("pr")}>
                 PR
               </button>
             </li>
           </div>
         </nav>
         <nav className="navbar navbar-expand" id="header">
-          <Link to={'#'} className="navbar-brand">
+          <Link to={"#"} className="navbar-brand">
             <img alt="logo" className="logo-header" src={logo} />
           </Link>
           <div className="navbar-nav mr-auto mobile-nav">
@@ -115,7 +115,7 @@ class App extends Component {
             </li> */}
             {showModeratorBoard && (
               <li className="nav-item">
-                <Link to={'/app/mod'} className="nav-link">
+                <Link to={"/app/mod"} className="nav-link">
                   Winery Board
                 </Link>
               </li>
@@ -123,24 +123,24 @@ class App extends Component {
 
             {showAdminBoard && (
               <li className="nav-item">
-                <Link to={'/app/admin'} className="nav-link">
+                <Link to={"/app/admin"} className="nav-link">
                   Admin Board
                 </Link>
               </li>
             )}
-            {/* 
+
             {currentUser && (
               <li className="nav-item btn-secondary tasting">
                 <Link to={"/app/user"} className="nav-link">
                   {i18next.t("Tastings")}
                 </Link>
               </li>
-            )} */}
+            )}
           </div>
           {currentUser ? (
             <div className="navbar-nav ml-auto mobile-nav">
               <li className="nav-item">
-                <Link to={'/app/profile'} className="nav-link">
+                <Link to={"/app/profile"} className="nav-link">
                   <i className="far fa-user-circle"></i> {currentUser.username}
                 </Link>
               </li>
@@ -153,17 +153,17 @@ class App extends Component {
           ) : (
             <div className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link to={'/app/login'} className="nav-link">
+                <Link to={"/app/login"} className="nav-link">
                   <button className="btn btn-primary btn-block">
-                    {i18next.t('Login')}
+                    {i18next.t("Login")}
                   </button>
                 </Link>
               </li>
 
               <li className="nav-item">
-                <Link to={'/app/register'} className="nav-link">
+                <Link to={"/app/register"} className="nav-link">
                   <button className="btn btn-secondary btn-block">
-                    {i18next.t('Register')}
+                    {i18next.t("Register")}
                   </button>
                 </Link>
               </li>
@@ -172,8 +172,8 @@ class App extends Component {
         </nav>
         <div className="container mt-3">
           <Switch>
-            <Route exact path={'/'} component={Home} />
-            <Route exact path={'/:id'} component={Home} />
+            <Route exact path={"/"} component={Home} />
+            <Route exact path={"/:id"} component={Home} />
             <Route exact path="/app/login" component={Login} />
             <Route exact path="/app/login/:id" component={Login} />
             <Route exact path="/app/register/:id" component={Register} />
